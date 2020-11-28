936b17535280f575445c425ee3253922a356dd69
Mohammed Alghaili <mohammed.alghaili94@gmail.com>
2020-11-28 23:12:53 +0300

    make it better

--- show.php
+++ show.php
@@ -1,8 +1,8 @@
 <?php
-$name_host="localhost";
-$name_user="root";
-$pass="";
-$db="wesal_bd";
+$name_host="sql12.freemysqlhosting.net";
+$name_user="sql12379002";
+$pass="Zf3G4bRBag";
+$db="sql12379002";
 $con=mysqli_connect($name_host,$name_user,$pass,$db);
 
 if(isset($_POST['btn'])=='add')
